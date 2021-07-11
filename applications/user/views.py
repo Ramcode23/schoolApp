a92b29457ca00136a0cca7d320643f048ed3077a
Ramc234 <=moyeto3@gmail.com>
2021-07-11 18:48:38 -0400

    Refactoring Permissions and authentications user

--- applications/user/views.py
+++ applications/user/views.py
@@ -6,7 +6,7 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ViewSet, ModelViewSet
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from .permission import IsAdminUser, IsLoggedInUserOrAdmin, IsAdminOrAnonymousUser
 from .models import User
 from .serializers import UserSerializer
@@ -15,18 +15,20 @@ from .serializers import UserSerializer
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    authentication_classes = [TokenAuthentication]
+
+    def get_authenticators(self):
+       if  self.request.method!='POST':
+          self.authentication_classes.append(TokenAuthentication)
+       return [auth() for auth in self.authentication_classes]
 
     def get_permissions(self):
-        permission_classes = []
-        if self.action == 'create':
+        permission_classes = []    
+        if self.action == 'list':
             permission_classes = [IsAdminUser]
-        elif self.action == 'list':
-            permission_classes = [IsAdminOrAnonymousUser]
         elif self.action == 'retrieve' or self.action == 'update' or self.action == 'partial_update':
-            permission_classes = [IsLoggedInUserOrAdmin]
+            permission_classes = [IsAuthenticated]
         elif self.action == 'destroy':
-            permission_classes = [IsLoggedInUserOrAdmin]
+            permission_classes = [IsAdminUser]
         return [permission() for permission in permission_classes]
 
 
@@ -34,8 +36,6 @@ class LoginView(ViewSet):
     serializer_class = AuthTokenSerializer
 
     def create(self,request):
-        print('create')
-        print(request)
         return ObtainAuthToken().as_view()(request=request._request)
 
 
@@ -43,3 +43,6 @@ class LogoutView(APIView):
     def get(self, request, format=None):
         request.user.auth_token.delete()
         return Response(status=status.HTTP_200_OK)
+    
+    
+    
